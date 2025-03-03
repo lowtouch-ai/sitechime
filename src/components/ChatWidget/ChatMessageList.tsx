07dfbf216ba0b56e92cba5663ab9fd0bde7aa94e
Sachin <sachin.george@pcpl.io>
2025-03-03 17:46:45 +0530

    user alignment to right

--- src/components/ChatWidget/ChatMessageList.tsx
+++ src/components/ChatWidget/ChatMessageList.tsx
@@ -55,7 +55,7 @@ export const ChatMessageList: React.FC = () => {
             </div>
           )}
           
-          <div className="whitespace-pre-wrap text-sm text-left">
+          <div className={`whitespace-pre-wrap text-sm ${msg.role === 'user' ? 'text-right' : 'text-left'}`}>
             {msg.content}
           </div>
         </div>
